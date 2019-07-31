248f944571205b3dfdf02432000432d49fd10cfc
unknown <dbdass033@gmail.com>
2019-07-31 08:05:48 +0530

    add babel,css loader into project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-const sum=require('./sum');
+import sum from './sum';
+import './image-viewer';
 const total=sum(10,5);
 console.log(total)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,17 @@ entry:'./src/index.js',
 output:{
     path:path.resolve(__dirname,'build'),
     filename:'bundle.js'
+},
+module:{
+    rules:[
+        {use:'babel-loader',
+        test:/\.js$/
+    },{
+        use:['style-loader','css-loader'],
+        test:/\.css$/
+    }
+
+    ]
 }
 
 
